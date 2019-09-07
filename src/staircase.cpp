e09693576447cc9b190ebf4fc8d9db6a114e3b65
gonmiermu <gonzalo_skater94@hotmail.com>
2019-09-07 14:42:15 +0200

    First day in Hackerrank

---
+++ src/staircase.cpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////
+//                                                               //
+//    https://www.hackerrank.com/challenges/staircase/problem   //
+//                                                               //
+///////////////////////////////////////////////////////////////////
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// Complete the staircase function below.
+void staircase(int n) {
+for( int i = 1; i <= n; i++ )
+{
+    for(int j= 0; j<(n-i); j++)
+        cout << " ";
+    for(int j= 0; j<i; j++)
+        cout << "#";
+    cout << endl;
+}
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    staircase(n);
+
+    return 0;
+}
